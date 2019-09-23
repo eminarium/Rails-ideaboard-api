241de392c647e9e5c765c994823eefa340e13938
Merdan Durdiyev <merdan.durdiyev@gmail.com>
2019-09-23 19:13:22 +0500

    Configured CORS settings

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,14 @@ module IdeaboardApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3001'
+        resource '*', :headers => :any, :methods => [:get, :put, :post, :delete, :options]
+      end
+    end
+
   end
 end
